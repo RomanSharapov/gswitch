483b77ec925bea2afffffef2c67fdda8e9c4467b
Roman Sharapov <Roman_Sharapov@epam.com>
2019-08-02 16:50:19 +0300

    Change code format. Tabs to Spaces and minor identation fix

--- gswitch_test.go
+++ gswitch_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestEmpty(t *testing.T) {
-	var s string = ""
-	e := empty(s)
-	if !e {
-		t.Error("Expected true, got ", e)
-	}
+    var s string = ""
+    e := empty(s)
+    if !e {
+        t.Error("Expected true, got ", e)
+    }
 }
